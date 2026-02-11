290113f98b02e92646b31e1c8fb06f2e26af6e85
SpriNt-PL <kubator1000@gmail.com>
2026-02-11 22:32:54 +0100

    Pierwszy commit. Dodana inicjalizacja uwierzytelnienia

---
+++ script.py
@@ -0,0 +1,20 @@
+import requests
+
+PROD_URL = "https://api.ksef.mf.gov.pl/v2"
+
+def inicjacja_uwierzytelniania():
+    challange_url = f"{PROD_URL}/auth/challenge"
+
+    response = requests.post(challange_url)
+    response.raise_for_status()
+
+    challenge_data = response.json()
+    challenge_str = challenge_data['challenge']
+    timestamp = challenge_data['timestamp']
+
+    print(f"Sukces! Otrzymano challenge: {challenge_str}")
+    print(f"Timestamp serwera: {timestamp}")
+
+if __name__ == "__main__":
+    print("Program wystartował.")
+    inicjacja_uwierzytelniania()
