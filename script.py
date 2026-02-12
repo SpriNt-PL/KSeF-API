c1d4869c7bd40ee043fa19c83ecb0f3a8e4d7cd8
Kuba <kuba@example.com>
2026-02-12 18:20:47 +0100

    Przed pobieraniem certyfikatu

--- script.py
+++ script.py
@@ -21,14 +21,39 @@ def inicjacja_uwierzytelniania():
 
     return challenge_str, timestamp
 
-def uwierzytelnianie_z_tokenem(challange, timestamp):
-    return
+def uwierzytelnianie_z_tokenem(nip, token, challange, timestamp):
+
+    url = "https://api.ksef.mf.gov.pl/v2/auth/ksef-token"
+
+    query_payload = {
+        "challenge": f"{challange}",
+        "contextIdentifier": {
+            "type": "Nip",
+            "value": f"{nip}"
+        },
+        "encryptedToken": f"{token}|{timestamp}"
+    }
+
+    response = requests.post(url, json=query_payload)
+    print(f"Response code: {response.status_code}")
+
+    if response.status_code != 202:
+        response_data = response.json()
+
+        print(response_data)
+
 
 if __name__ == "__main__":
-    print("Program wystartował.")
+    print("Program wystartował.\n")
+    
+    print("1. Inicjacja uwierzytelniania")
     challange, timestamp = inicjacja_uwierzytelniania()
 
     load_dotenv()
 
     nip = os.getenv("NIP")
-    print(nip)
+    token = os.getenv("TOKEN") 
+    print(f"\nNIP = {nip}, TOKEN = {token}")
+
+    print("2. Uwierzytelnianie tokenem")
+    uwierzytelnianie_z_tokenem(nip, token, challange, timestamp)
