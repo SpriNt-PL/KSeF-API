8e4bc8c746446d063c60c03b8fe5ead486aa3042
Kuba <kuba@example.com>
2026-02-12 18:39:03 +0100

    Pobieranie certyfikatu działa

--- script.py
+++ script.py
@@ -5,9 +5,9 @@ from dotenv import load_dotenv
 PROD_URL = "https://api.ksef.mf.gov.pl/v2"
 
 def inicjacja_uwierzytelniania():
-    challange_url = f"{PROD_URL}/auth/challenge"
+    url = f"{PROD_URL}/auth/challenge"
 
-    response = requests.post(challange_url)
+    response = requests.post(url)
     response.raise_for_status()
 
     print(f"Response code: {response.status_code}")
@@ -21,9 +21,22 @@ def inicjacja_uwierzytelniania():
 
     return challenge_str, timestamp
 
+
+def pobieranie_certyfikatow():
+    
+    url = f"{PROD_URL}/security/public-key-certificates"
+
+    response = requests.get(url)
+
+    response_data = response.json()
+    response_data = response_data[0]
+
+    return response_data['certificate']
+
+
 def uwierzytelnianie_z_tokenem(nip, token, challange, timestamp):
 
-    url = "https://api.ksef.mf.gov.pl/v2/auth/ksef-token"
+    url = f"{PROD_URL}/auth/ksef-token"
 
     query_payload = {
         "challenge": f"{challange}",
@@ -49,11 +62,13 @@ if __name__ == "__main__":
     print("1. Inicjacja uwierzytelniania")
     challange, timestamp = inicjacja_uwierzytelniania()
 
+    print("\n2. Pobieranie ")
+    pobieranie_certyfikatow()
+
     load_dotenv()
 
     nip = os.getenv("NIP")
     token = os.getenv("TOKEN") 
-    print(f"\nNIP = {nip}, TOKEN = {token}")
 
-    print("2. Uwierzytelnianie tokenem")
+    print(f"\n3. Uwierzytelnianie tokenem (NIP = {nip} oraz TOKEN = {token})")
     uwierzytelnianie_z_tokenem(nip, token, challange, timestamp)
