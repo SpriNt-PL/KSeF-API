8f4b4112ef1d19d260cbf66a04caa805ceb2f369
Kuba <kuba@example.com>
2026-02-13 13:18:09 +0100

    Zrobiłem funkcję do eksportu i szyfrowania ale mam ciągle błąd 403

--- script.py
+++ script.py
@@ -35,6 +35,8 @@ def pobieranie_certyfikatow():
     response_data = response.json()
     response_data = response_data[0]
 
+    print(f"Certyfikat ważny do {response_data['validTo']}")
+
     return response_data['certificate']
 
 
@@ -73,13 +75,70 @@ def uwierzytelnianie_z_tokenem(nip, challange, encrypted_token):
     response = requests.post(url, json=query_payload)
     print(f"Response code: {response.status_code}")
 
-    response_data = response.json()
-    print(response_data)
+    if response.status_code == 202:
+        response_data = response.json()
+        print(f"Token ważny do: {response_data['authenticationToken']['validUntil']}")
+        return response_data['authenticationToken']['token']
 
 
-    if response.status_code != 202:
+    else:
         print(response_data)
 
+        return None
+
+
+def szyfrowanie_eksportu(certificate):
+    symmetric_key = os.urandom(32)
+
+    initialization_vector = os.urandom(16)
+
+    cert_bytes = base64.b64decode(certificate)
+    cert_obj = x509.load_der_x509_certificate(cert_bytes)
+    public_key = cert_obj.public_key()
+
+    encrypted_key = public_key.encrypt(symmetric_key, padding.OAEP(
+        mgf=padding.MGF1(algorithm=hashes.SHA256()),
+        algorithm=hashes.SHA256(),
+        label=None
+    ))
+
+    encrypted_key_b64 = base64.b64encode(encrypted_key).decode('utf-8')
+    initialization_vector_b64 = base64.b64encode(initialization_vector).decode('utf-8')
+
+    return encrypted_key_b64, initialization_vector_b64, symmetric_key, initialization_vector
+
+
+def eksport_faktur(encrypted_key_b64, initialization_vector_b64, session_token):
+
+    url = f"{PROD_URL}/invoices/exports"
+
+    headers = {
+        "Authorization": f"Bearer {session_token}"
+    }
+
+    query_payload = {
+        "encryption": {
+            "encryptedSymmetricKey": encrypted_key_b64,
+            "initializationVector": initialization_vector_b64
+        },
+        "filters": {
+            "subjectType": "Subject2", 
+            "dateRange": {
+                "dateType": "Invoicing",
+                "from": "2026-02-01T00:00:00Z",
+                #"to": "2026-02-13T23:59:59Z"
+            }
+        }
+    }
+
+    response = requests.post(url, headers=headers, json=query_payload)
+
+    print(f"Response code: {response.status_code}")
+
+    if response.status_code == 201:
+        print(response.json())
+    else:
+        print(response.text)
 
 if __name__ == "__main__":
     print("Program wystartował.\n")
@@ -87,7 +146,7 @@ if __name__ == "__main__":
     print("1. Inicjacja uwierzytelniania")
     challange, timestamp = inicjacja_uwierzytelniania()
 
-    print("\n2. Pobieranie ")
+    print("\n2. Pobieranie certyfikatow")
     certificate = pobieranie_certyfikatow()
 
     load_dotenv()
@@ -96,5 +155,12 @@ if __name__ == "__main__":
     token = os.getenv("TOKEN") 
 
     print(f"\n3. Uwierzytelnianie tokenem (NIP = {nip} oraz TOKEN = {token})")
+
     encrypted_token = szyfrowanie_encryptedToken(token, timestamp, certificate)
-    uwierzytelnianie_z_tokenem(nip, challange, encrypted_token)
+    session_token = uwierzytelnianie_z_tokenem(nip, challange, encrypted_token)
+
+
+    print("\n4. Pobieranie faktur")
+
+    encrypted_key_b64, initialization_vector_b64, symmetric_key, initialization_vector = szyfrowanie_eksportu(certificate)
+    eksport_faktur(encrypted_key_b64, initialization_vector_b64, session_token)
