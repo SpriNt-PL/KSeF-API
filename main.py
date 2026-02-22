b8603c28d8898c5d89f34dcccf860aed98309884
Kuba <kuba@example.com>
2026-02-22 11:38:38 +0100

    Preparing code for first exe file

--- main.py
+++ main.py
@@ -2,6 +2,14 @@ from directories_preparation import prepare_directories
 from ksef_api import download_invoices
 from invoice_preparation import prepare_invoices
 
-prepare_directories()
-download_invoices()
-prepare_invoices()
+if __name__ == "__main__":
+
+    try:
+        prepare_directories()
+        download_invoices()
+        prepare_invoices()
+    except Exception as e:
+        print(f"Error {e}")
+    finally:
+        print("\nEnd of a program")
+        input("Press Enter to close the program")
