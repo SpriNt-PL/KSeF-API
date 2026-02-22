62852746bcc65e7b6592898bc08d294157b157fd
Kuba <kuba@example.com>
2026-02-22 11:21:04 +0100

    Added auxiliary_functions.py

---
+++ auxiliary_functions.py
@@ -0,0 +1,8 @@
+import os
+import sys
+
+def get_base_path():
+    if getattr(sys, 'frozen', False):
+        return os.path.dirname(sys.executable)
+    else:
+        return os.path.dirname(os.path.abspath(__file__))
