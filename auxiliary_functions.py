18e1137775734c755029b462ef1c53c00dcdde30
Kuba <kuba@example.com>
2026-02-23 18:45:11 +0100

    Now exe file is starting correctly on my own pc

--- auxiliary_functions.py
+++ auxiliary_functions.py
@@ -1,8 +1,23 @@
 import os
 import sys
+import subprocess
 
 def get_base_path():
     if getattr(sys, 'frozen', False):
         return os.path.dirname(sys.executable)
     else:
         return os.path.dirname(os.path.abspath(__file__))
+    
+
+def prepare_playwright():
+    browsers_path = os.path.join(os.environ['LOCALAPPDATA'], 'ms-playwright')
+
+    os.environ['PLAYWRIGHT_BROWSERS_PATH'] = browsers_path
+
+    if not os.path.exists(browsers_path) or not os.listdir(browsers_path):
+        try:
+            subprocess.run([sys.executable, "-m", "playwright", "install", "chromium"], check=True)
+            print("Browser engine installed successfully")
+        except Exception as e:
+            print(f"Installation error: {e}")
+            sys.exit(1)
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from directories_preparation import prepare_directories
 from ksef_api import download_invoices
 from invoice_preparation import prepare_invoices
+from auxiliary_functions import prepare_playwright
 
 if __name__ == "__main__":
 
     try:
+        prepare_playwright()
         prepare_directories()
         download_invoices()
         prepare_invoices()
